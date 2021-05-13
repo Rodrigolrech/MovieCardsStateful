0653acdca88947b2b24b6008e41535078e22254e
Rodrigo Rech <rodrigorech92@gmail.com>
2021-05-13 10:34:06 -0300

    SearchBar renderized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import SearchBar from './components/SearchBar';
 import Header from './components/Header';
+import movies from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar movies={ movies } />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,11 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <h1>searchBar</h1>
+    );
+  }
+}
